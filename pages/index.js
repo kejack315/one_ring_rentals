1eb40d5668647f1d478bb799e661f2da1fe6a610
zke <zke920407@gmail.com>
2023-10-14 21:41:19 +0800

    Initial

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,45 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Header from "../components/header";
+import CarouselSlide from "../components/carousel slide";
+import HomeAdvancedSearch from "../components/home-advanced-search";
+import Content from "../components/content";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div >
+        <Head>
+            <meta charSet="utf-8" />
+            <title>One Ring Rentals - Home</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+            <link
+                href="http://fonts.googleapis.com/css?family=Raleway:300,500,900%7COpen+Sans:400,700,400italic"
+                rel="stylesheet"
+                type="text/css"
+            />
+            <link href="css/bootstrap.min.css" rel="stylesheet" />
+            <link href="css/style.css" rel="stylesheet" />
+            <script src="../public/js/common/modernizr.js"></script>
+        </Head>
+        <Header >
+        </Header>
+        <CarouselSlide>
+        </CarouselSlide>
+        <HomeAdvancedSearch>
+        </HomeAdvancedSearch>
+        <Content></Content>
+        <Footer></Footer>
+        <script src="../public/js/common.js"></script>
+        <script type="text/javascript" src="../public/js/common/jquery-1.11.1.min.js"></script>
+        <script type="text/javascript" src="../public/js/common/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../public/js/common/bootstrap-datepicker.js"></script>
+        <script type="text/javascript" src="../public/js/common/chosen.min.js"></script>
+        <script type="text/javascript" src="../public/js/common/bootstrap-checkbox.js"></script>
+        <script type="text/javascript" src="../public/js/common/nice-scroll.js"></script>
+        <script type="text/javascript" src="../public/js/common/jquery-browser.js"></script>
+        <script type="text/javascript" src="../public/js/scripts.js"></script>
     </div>
   );
 }
